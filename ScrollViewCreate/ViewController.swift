d58eba51b150b149cf46da6fecdcaecd49793982
Di1105 <delcioglu@gmail.com.com>
2022-12-23 15:15:09 +0300

    Commit

--- ScrollViewCreate/ViewController.swift
+++ ScrollViewCreate/ViewController.swift
@@ -14,56 +14,78 @@ class ViewController: UIViewController {
     var scrollView = UIScrollView()
     var firstLabel = UILabel()
     var secondLabel = UILabel()
-
-
+    var thirdLabel = UILabel()
+    var fourthLabel = UILabel()
+    var imageView = UIImageView()
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setUpUI()
 
     }
-
+    
     func setUpUI(){
         view.addSubview(scrollView)
-        view.addSubview(firstLabel) //
+        view.addSubview(firstLabel)
         view.addSubview(secondLabel)
-       
-        
-        
+        view.addSubview(thirdLabel)
+    
         
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.snp.makeConstraints { make in
-            make.centerX.centerY.height.width.equalToSuperview()
+        make.top.bottom.leading.trailing.equalToSuperview()
+        
         }
-       // scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        //scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
         scrollView.backgroundColor = .systemYellow
-        scrollView.contentSize = CGSize(width: view.frame.size.width, height: scrollView.frame.size.height*2)
+        scrollView.contentSize = CGSize(width: view.frame.size.width, height: view.frame.size.height*2)
         scrollView.isPagingEnabled = true
         
-        
+        //FIRST LABEL-------------------------------------
         firstLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(80)
-            make.centerXWithinMargins.equalTo(scrollView.snp.centerXWithinMargins)
-            make.width.equalTo(150)
-            make.height.equalTo(40)
+        make.top.equalToSuperview().offset(300)
+        make.centerXWithinMargins.equalToSuperview()
+        make.width.equalTo(100)
+        make.height.equalTo(40)
         }
         firstLabel.backgroundColor = .purple
         firstLabel.text = "PURPLE"
         firstLabel.textColor = .white
         firstLabel.textAlignment = .center
+        scrollView.addSubview(firstLabel)
         
+        //SECOND LABEL-----------------------------------
         secondLabel.snp.makeConstraints { make in
-            make.top.equalTo(firstLabel.snp.bottom).offset(100)
-            make.centerXWithinMargins.equalToSuperview()
-            make.width.equalTo(150)
-            make.height.equalTo(40)
+        make.bottom.equalTo(firstLabel.snp.bottom).offset(300)
+        make.centerXWithinMargins.equalToSuperview()
+        make.width.equalTo(100)
+        make.height.equalTo(40)
         }
         secondLabel.backgroundColor = .systemPink
         secondLabel.text = "PINK"
         secondLabel.textColor = .white
         secondLabel.textAlignment = .center
         scrollView.addSubview(secondLabel)
+        
+        
+        //THIRD LABEL-------------------------------------
+        thirdLabel.snp.makeConstraints { make in
+        make.top.equalTo(secondLabel.snp.bottom).offset(300)
+        make.centerXWithinMargins.equalToSuperview()
+        make.width.equalTo(100)
+        make.height.equalTo(40)
+        }
+        thirdLabel.backgroundColor = .systemOrange
+        thirdLabel.text = "ORANGE"
+        thirdLabel.textColor = .white
+        thirdLabel.textAlignment = .center
+        scrollView.addSubview(thirdLabel)
     }
-
-
+            
+    
 }
 
